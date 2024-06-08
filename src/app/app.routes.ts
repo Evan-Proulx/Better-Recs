72825eab9c9da1eacb0a10ff3b52109ad7e2f246
eproulx <w0816747@myscc.ca>
2024-06-08 18:33:25 -0400

    -Remove keys from enviroment file

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,7 +1,7 @@
 import { Routes } from '@angular/router';
+import {TestComponent} from "./test/test.component";
 import {MainComponent} from "./main/main.component";
 import {AuthPageComponent} from "./authentication/auth-page/auth-page.component";
-import {TestComponent} from "./test/test.component";
 
 export const routes: Routes = [
   {path: '', component: MainComponent},
